f07acdd1a2e451b5e957478eac99236ee2ad4731
rrd108 <rrd@webmania.cc>
2023-08-27 19:19:39 +0200

    the game implemented

--- src/components/Puzzle.vue
+++ src/components/Puzzle.vue
@@ -9,12 +9,51 @@
   const solution = ref(textShuffled)
 
   const spannedSolution = computed(() => solution.value.split(''))
+
+  const oldChar = ref('')
+  const newChar = ref('')
+  const clickedPosition = ref(-1)
+
+  const setValuesForChange = (i: number) => {
+    clickedPosition.value = i
+    oldChar.value = textShuffled[i]
+  }
+
+  const isCorrectGuess = () => newChar.value == secret[clickedPosition.value]
+
+  const getCharPositions = (oldChar: string) => {
+    const positions = []
+    for (let i = 0; i < textShuffled.length; i++) {
+      if (textShuffled[i] == oldChar) {
+        positions.push(i)
+      }
+    }
+    return positions
+  }
+
+  const changeChar = () => {
+    if (isCorrectGuess()) {
+      const positions = getCharPositions(oldChar.value)
+      positions.forEach(
+        position =>
+          (solution.value = solution.value.substring(0, position) + newChar.value + solution.value.substring(position + 1))
+      )
+      clickedPosition.value = -1
+    }
+    oldChar.value = ''
+    newChar.value = ''
+  }
 </script>
 
 <template>
   <h1>Vue BDD DeCrypt Game</h1>
+  <div v-if="clickedPosition != -1">Helytelen tipp!</div>
+  <div v-if="oldChar">
+    <h6>Cseréljük a "{{ oldChar }}" karaktert erre:</h6>
+    <input type="text" v-model="newChar" @input="changeChar" />
+  </div>
   <h2>
-    <span v-for="(char, i) in spannedSolution" :key="i">
+    <span v-for="(char, i) in spannedSolution" :key="i" @click="setValuesForChange(i)" :class="{ good: char == secret[i] }">
       {{ char }}
     </span>
   </h2>
@@ -23,5 +62,30 @@
 <style scoped>
   h2 {
     font-family: 'Courier New', Courier, monospace;
+  }
+  span {
+    cursor: pointer;
+    background-color: #ee9;
+    color: #aaa;
+  }
+  .good {
+    background: none;
+    color: #eee;
+  }
+  div {
+    position: absolute;
+    top: 0;
+    background-color: rgb(41, 0, 49);
+    padding: 1em;
+    font-size: 2rem;
+  }
+  h6 {
+    margin: 0;
+  }
+  input {
+    display: inline;
+    width: 2em;
+    text-align: center;
+    font-size: 2rem;
   }
 </style>
--- src/test/coponents/Puzzle.test.ts
+++ src/test/coponents/Puzzle.test.ts
@@ -1,6 +1,8 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Puzzle from '../../components/Puzzle.vue'
+
+vi.mock('../../assets/text.ts', () => ({ default: 'Gauranag rrd!' }))
 
 describe('Puzzle Component', () => {
   it('shoud load the Puzzle component', () => {
@@ -12,5 +14,35 @@ describe('Puzzle Component', () => {
     const componentData = wrapper.vm
 
     expect(componentData.solution).toHaveLength(componentData.secret.length)
+    expect(componentData.solution).toHaveLength(13)
+    expect(componentData.clickedPosition).toBe(-1)
+
+    // TODO expect the component dispaly the shuffled text
+  })
+
+  it('should react to cliks', async () => {
+    const wrapper = mount(Puzzle, {})
+    const componentData = wrapper.vm
+
+    const span = wrapper.get('span')
+    span.trigger('click')
+    expect(componentData.clickedPosition).toBe(0)
+    expect(componentData.oldChar).toBe(span.text())
+
+    await wrapper.vm.$nextTick()
+    expect(wrapper.get('input').isVisible()).toBe(true)
+  })
+
+  it('should handle the input', async () => {
+    const wrapper = mount(Puzzle, {})
+    wrapper.get('span').trigger('click')
+    await wrapper.vm.$nextTick()
+
+    const input = wrapper.get('input')
+    const typedChar = wrapper.vm.secret[0]
+    input.setValue(typedChar)
+    input.trigger('input')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.solution[0]).toBe(typedChar)
   })
 })
